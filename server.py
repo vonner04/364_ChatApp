d110e888ba51951e1c5792244c42dc4e00dcc834
vonner04 <randyleean092m@gmail.com>
2024-10-07 22:54:47 +1300

    feature: setup basic login/registration steps for user.

--- server.py
+++ server.py
@@ -1,39 +1,46 @@
-import sqlite3
-import bcrypt
+import socket
+import threading
+from database.client_manager import create_user_table, login_user
+
+PORT = 5555
+SERVER = socket.gethostbyname(socket.gethostname())
 
 
-def create_user_table():
-    conn = sqlite3.connect("example.db")
-    cur = conn.cursor()
-    cur.execute(
-        """CREATE TABLE IF NOT EXISTS users(
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                username VARCHAR(255) NOT NULL UNIQUE,
-                password VARCHAR(255) NOT NULL
-                )
-            """
-    )
+# Initialize the server
+def start():
+    create_user_table()
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((SERVER, PORT))
+    server_socket.listen(5)
+    print(f"Server is listening on {SERVER}")
 
-    conn.commit()
-    conn.close()
-
-
-def register_user(username, password):
-    conn = sqlite3.connect("example.db")
-    cur = conn.cursor()
-    cur.execute(
-        """INSERT INTO users(username, password) VALUES(?, ?)""",
-        (username, hash_password(password)),
-    )
-
-    conn.commit()
-    conn.close()
+    while True:
+        client_socket, address = server_socket.accept()
+        print(f"Connection from {address} has been established.")
+        client_handler = threading.Thread(target=handle_client, args=(client_socket,))
+        client_handler.start()
 
 
-def hash_password(password):
-    # Convert password to array of bytes and hash it
-    return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+def handle_client(client_socket):
+    # Prompt user to login with username and password
+    client_socket.send("[SERVER] Enter username: ".encode("utf-8"))
+    username = client_socket.recv(1024).decode("utf-8")
+    client_socket.send("[SERVER] Enter password: ".encode("utf-8"))
+    password = client_socket.recv(1024).decode("utf-8")
+
+    # Login user
+    response = login_user(username, password)
+    client_socket.send(response.encode("utf-8"))
+
+    # Handle user requests
+    while True:
+        request = client_socket.recv(1024).decode("utf-8")
+
+        if request == "exit":
+            break
+
+    client_socket.close()
 
 
-def check_password(password, hashed_password):
-    return bcrypt.checkpw(password.encode("utf-8"), hashed_password)
+if __name__ == "__main__":
+    start()
